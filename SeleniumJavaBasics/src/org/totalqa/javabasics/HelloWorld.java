86b922df24d1264445fd0405bb5b10d9feb0d681
totalqa9 <36955052+totalqa9@users.noreply.github.com>
2018-08-30 18:51:02 +0530

    Adding a statement to print Selenium in the console
    This example is useful to learn to resolve the conflicts in Git-hub.
    Right now we are committing directly into the master branch without any review and pull request. 
    But in real time these changes might have been introduced into the master branch via pull request from another branch.

--- SeleniumJavaBasics/src/org/totalqa/javabasics/HelloWorld.java
+++ SeleniumJavaBasics/src/org/totalqa/javabasics/HelloWorld.java
@@ -2,10 +2,9 @@ package org.totalqa.javabasics;
 
 public class HelloWorld 
 {
-
 	public static void main(String[] args) 
 	{
-		
 		System.out.println("Hello World");
+		System.out.println("Selenium");
 	}
 }
